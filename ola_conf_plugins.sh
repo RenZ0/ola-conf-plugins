32c4e941afdecf9749349cb6e32252a83e8ff134
Laurent Pierru <renzo@imaginux.com>
2012-09-05 11:18:26 +0200

    initial v1c

---
+++ ola_conf_plugins.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# GPL-RenZO-2012
+
+cd ~/.ola
+
+conf_list='
+ola-artnet.conf
+ola-dummy.conf
+ola-e131.conf
+ola-espnet.conf
+ola-ftdidmx.conf
+ola-opendmx.conf
+ola-pathport.conf
+ola-sandnet.conf
+ola-shownet.conf
+ola-stageprofi.conf
+ola-usbdmx.conf
+ola-usbserial.conf
+'
+
+if [ $# != 2 ]
+then
+	echo '---'
+	echo 'settings path is:'
+	pwd
+	echo '---'
+	echo 'options are: show/status/enable/disable all/pattern'
+	echo 'for example: show all, status all, enable all,'
+	echo 'show e131, enable usb, disable net, enable artnet...'
+	echo '---'
+else
+	# if all, we set conf to match all
+	if [ $2 = 'all' ]
+	then
+		exp='conf'
+	else
+		exp=$2
+	fi
+
+	# matching results
+	new_conf_list=`ls $conf_list | grep $exp`
+
+	for file in $new_conf_list
+	do
+
+		#SHOW
+		if [ $1 = 'show' ]
+		then
+			echo --- $file ---
+			cat $file
+			echo ---
+		fi
+
+		#STATUS
+		if [ $1 = 'status' ]
+		then
+			echo --- $file ---
+			# if enabled present, show the line
+			if [ `cat $file | grep enabled | wc -l` != 0 ]
+			then
+				cat $file | grep enabled
+			else
+				echo 'status missing'
+			fi
+		fi
+
+		#ENABLE/DISABLE
+		if [ $1 = 'enable' ] || [ $1 = 'disable' ]
+		then
+			# if enabled present, change the line, else add the line
+			if [ `cat $file | grep enabled | wc -l` != 0 ]
+			then
+				if [ $1 = 'enable' ]
+				then
+					sed "s/enabled = false/enabled = true/" $file > $file.tmp && mv $file.tmp $file
+					echo activate $file
+				else
+					sed "s/enabled = true/enabled = false/" $file > $file.tmp && mv $file.tmp $file
+					echo deactivate $file
+				fi
+			else
+				if [ $1 = 'enable' ]
+				then
+					echo 'enabled = true' >> $file
+				else
+					echo 'enabled = false' >> $file
+				fi
+					echo status added to $file
+			fi
+		fi
+
+	done
+fi
